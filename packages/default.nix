31c39b5440194d60ac48e3f8e71dcf6ef4db9de9
Denis Craig <admin@deniscraig.com>
2025-05-11 15:16:28 +0100

    Load source from local folder instead of fetching from github

--- packages/default.nix
+++ packages/default.nix
@@ -2,20 +2,13 @@
 pkgs.stdenv.mkDerivation {
   pname = "zide";
   version = "3.2.0";
-
-  src = pkgs.fetchFromGitHub {
-    owner = "denis101";
-    repo = "zide";
-    rev = "main";
-    sha256 = "sha256-Ten0Jme1wVDsUhRY/ggPY1fKwYRw0FiQWSQqVij3nHI";
-  };
-
+  src = ./..;
   installPhase = ''
     mkdir -p $out/{bin,layouts,lf,yazi}
-    cp -r $src/bin/* $out/bin/
-    cp -r $src/layouts/* $out/layouts/
-    cp -r $src/lf/* $out/lf/
-    cp -r $src/yazi/* $out/yazi/
+    cp -ar $src/bin/ $out/bin/
+    cp -ar $src/layouts/ $out/layouts/
+    cp -ar $src/lf/ $out/lf/
+    cp -ar $src/yazi/ $out/yazi/
     chmod -R +x $out/bin/
   '';
 }
